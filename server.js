e9eb44c3e4921a230c0a2f0220ea7ada7f6d9c98
Robyn <clockhands9@gmail.com>
2022-11-16 16:24:07 +0000

    basic listen

--- server.js
+++ server.js
@@ -1,2 +1,8 @@
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
+
+const app = express();
+const http = require('http').createServer(app);
+http.listen(3000, () => {
+    console.log('Listening on port 3000')
+})
